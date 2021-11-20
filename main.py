a2e0798f661211f1aa110c24893f6b4554e15ae5
Dima <dmitryl0626@gmail.com>
2021-11-20 14:55:03 +0100

    edit main example

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from qsim.qcircuit import QCircuit
+from qsim.qconstants import H
 
 circuit = QCircuit()
 
-circuit.addQubit(initValue=0)
+circuit.addQubits([0]) # create 1 qubit with init value 0
+circuit.addGates([H]) # add 1 column of gates
+circuit.addGates([H]) # add 2 column of gates
 
-circuit.addH(index=0)
+circuit.simulate() # make calculations
 
-state = circuit.measureAll()
-
-print(state)
+state = circuit.measureAll() # get state of all qubits
+print(state) # in this case output will be always 0
 
