c905d962d4b95840d03bc3ddbae05a6bdb6949b8
Dima <dmitryl0626@gmail.com>
2021-11-21 23:20:10 +0100

    add new example; Bell state

--- examples.py
+++ examples.py
@@ -36,5 +36,17 @@ circuit.simulate() # make calculations
 state = circuit.measureAll() # get state of all qubits
 print(state) # in this case output will be always [1, 0, 1]
 
+#EXAMPLE 4
+from qsim.qconstants import H, I
+circuit = QCircuit()
+
+circuit.addQubits([0, 0]) # create 2 qubits with init value 0
+circuit.addGates([H, I])  # add 1 column of gates
+circuit.addCNOT(controlIndex=0, targetIndex=1) # add CNOT gate with 0 index qubit as control and 1 index as target
+
+circuit.simulate() # make calculations
+state = circuit.measureAll() # get state of all qubits
+print(state) # in this case output will [0, 0] or [1, 1] with probability of 0.5
+
 
 
