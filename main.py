3b4150e9047733fcd11a5567e2b7e37a26fa934e
Dima <dmitryl0626@gmail.com>
2021-11-21 15:53:12 +0100

    main.py is using for testing only now

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
 from qsim.qcircuit import QCircuit
 
-#EXAMPLE 1
-from qsim.qconstants import H
 circuit = QCircuit()
 
-circuit.addQubits([0]) # create 1 qubits with init value 0
-circuit.addGates([H]) # add 1 column of gates
-circuit.addGates([H]) # add 2 column of gates
+circuit.addQubits([1, 0, 0])
+circuit.addCNOT(controlIndex=0, targetIndex=2) 
 
-circuit.simulate() # make calculations
-state = circuit.measureAll() # get state of all qubits
-print(state) # in this case output will be always 0
+circuit.simulate()
+state = circuit.measureAll()
+print(state)
 
-
-#EXAMPLE 2
-from qsim.qconstants import X, I
-circuit = QCircuit()
-
-circuit.addQubits([0, 0]) # create 2 qubits with init value 0
-circuit.addGates([I, X]) # add 1 column of gates
-circuit.addGates([X, X]) # add 2 column of gates
-
-circuit.simulate() # make calculations
-state = circuit.measureAll() # get state of all qubits
-print(state) # in this case output will be always [1, 0]
 
 
