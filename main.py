72e8ce502545303b3f1a92c8bcc08629d0f21732
Dima <dmitryl0626@gmail.com>
2021-11-26 21:40:07 +0100

    trying Deutsch-Jozsa Algorithm in main.py

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from qsim.qcircuit import QCircuit
-from qsim.qconstants import H, I
+from qsim.qconstants import H, I, X
 circuit = QCircuit()
 
-circuit.addQubits([0, 0])
-circuit.addGates([H, I])
-circuit.addCNOT(controlIndex=0, targetIndex=1) 
+circuit.addQubits([0, 0, 0, 1])
+circuit.addGates([H, H, H, H])
+
+
+# BEGIN Uf - function constant
+# circuit.addGate(X, 3)
+# END Uf
+
+# BEGIN Uf - function balanced
+circuit.addCNOT(controlIndex=0, targetIndex=3)
+circuit.addCNOT(controlIndex=1, targetIndex=3)
+circuit.addCNOT(controlIndex=2, targetIndex=3)
+# END Uf
+
+circuit.addGates([H, H, H, I])
 
 circuit.simulate()
 state = circuit.measureAll()
+
 print(state)
+if state[:-1] == [1, 1, 1]:
+    print("Function is balanced")
+else:
+    print("Function is constant")
 
 
 
